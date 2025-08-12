732a8fb48d3688f0366a127995bc925ce69b3b0e
atharvaBOT7 <atharvachundurwar841@gmail.com>
2025-08-11 19:02:22 -0700

    Basic application code added

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_tempate, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET","POST"])
+def home():
+    message = ""
+    if request.message == "POST":
+        name = request.form.get("name")
+        message = f"Hello {name}, Welcome to Kubernetes Test Application!"
+    return render_tempate("index.html", message=message)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port = 5001)
